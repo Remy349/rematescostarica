65e3800195913cf7c9ab41ee033530298f73ff79
Remy349 <santoraga15@gmail.com>
2022-07-21 20:29:25 -0600

    Opcion de editar el contenido de cada pagina, listo

--- flaskr/admin_content.py
+++ flaskr/admin_content.py
@@ -29,7 +29,83 @@ def agregar_contenido():
         elif from_page == "quienes_somos":
             redirect_page = "quienes_somos"
 
+        if session.get("is_edit") and session.get("is_edit_id") is None:
+            pass
+        else:
+            session.pop("is_edit", None)
+            session.pop("is_edit_id", None)
+
         return redirect(url_for(f"{redirect_page}"))
+
+@app.route("/contenido/editar/<int:content_page_id>", methods=["GET", "POST"])
+@login_required
+def editar_contenido(content_page_id):
+    """ Ruta/Funcion para editar el contenido de cada pagina del sitio """
+    username = session.get("username")
+
+    if username != os.getenv("ADMIN_USERNAME"):
+        return redirect(url_for("index"))
+    else:
+        if request.method == "GET":
+            if session.get("is_edit") and session.get("is_edit_id") is None:
+                session["is_edit"] = "true"
+                session["is_edit_id"] = content_page_id
+            else:
+                session.pop("is_edit", None)
+                session.pop("is_edit_id", None)
+
+                session["is_edit"] = "true"
+                session["is_edit_id"] = content_page_id
+
+            edit_content_page = ContentPage.query.filter_by(id=content_page_id).first()
+
+            redirect_page = None
+
+            if edit_content_page.from_page == "home":
+                redirect_page = "index"
+            elif edit_content_page.from_page == "quienes_somos":
+                redirect_page = "quienes_somos"
+
+            return redirect(url_for(f"{redirect_page}"))
+        elif request.method == "POST":
+            information_content = request.form["information_content"]
+
+            edit_content_page = ContentPage.query.filter_by(id=content_page_id).first()
+
+            redirect_page = None
+
+            if edit_content_page.from_page == "home":
+                redirect_page = "index"
+            elif edit_content_page.from_page == "quienes_somos":
+                redirect_page = "quienes_somos"
+
+            edit_content_page.information_content = information_content
+
+            db.session.add(edit_content_page)
+            db.session.commit()
+
+            session.pop("is_edit", None)
+            session.pop("is_edit_id", None)
+
+            return redirect(url_for(f"{redirect_page}"))
+
+@app.route("/contenido/cancelar/<int:content_page_id>", methods=["GET"])
+@login_required
+def cancelar_contenido(content_page_id):
+    """ Ruta/Funcion para cancelar el proceso de edicion de contenido """
+    cancel_content_page = ContentPage.query.filter_by(id=content_page_id).first()
+
+    redirect_page = None
+
+    if cancel_content_page.from_page == "home":
+        redirect_page = "index"
+    elif cancel_content_page.from_page == "quienes_somos":
+        redirect_page = "quienes_somos"
+
+    session.pop("is_edit", None)
+    session.pop("is_edit_id", None)
+
+    return redirect(url_for(f"{redirect_page}"))
 
 @app.route("/contenido/eliminar/<int:content_page_id>", methods=["GET"])
 @login_required
@@ -50,5 +126,11 @@ def eliminar_contenido(content_page_id):
 
         db.session.delete(delete_content_page)
         db.session.commit()
+
+        if session.get("is_edit") and session.get("is_edit_id") is None:
+            pass
+        else:
+            session.pop("is_edit", None)
+            session.pop("is_edit_id", None)
 
         return redirect(url_for(f"{redirect_page}"))
