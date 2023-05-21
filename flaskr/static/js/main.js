fdbe7b5f2eeba499f9e53d5b09e739fd18e8f187
Remy349 <santoraga15@gmail.com>
2023-05-21 17:42:15 -0600

    Added header and nav components.

---
+++ flaskr/static/js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const navMenu = document.getElementById('navMenu')
+  const navToggle = document.getElementById('navToggle')
+  const navClose = document.getElementById('navClose')
+
+  if (navToggle) {
+    navToggle.addEventListener('click', () => {
+      navMenu.classList.add('show-menu')
+    })
+  }
+
+  if (navClose) {
+    navClose.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu')
+    })
+  }
+})
