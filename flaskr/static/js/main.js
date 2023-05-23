89c50f486bf1e70e49b2d76ce1c7151d61264d0a
Remy349 <santoraga15@gmail.com>
2023-05-23 00:04:36 -0600

    Added about section.

--- flaskr/static/js/main.js
+++ flaskr/static/js/main.js
@@ -1,17 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const header = document.getElementById('header')
   const navMenu = document.getElementById('navMenu')
   const navToggle = document.getElementById('navToggle')
   const navClose = document.getElementById('navClose')
 
   if (navToggle) {
     navToggle.addEventListener('click', () => {
+      header.classList.remove('scrolling-header')
       navMenu.classList.add('show-menu')
     })
   }
 
   if (navClose) {
     navClose.addEventListener('click', () => {
+      header.classList.add('scrolling-header')
       navMenu.classList.remove('show-menu')
+    })
+  }
+
+  if (header) {
+    window.addEventListener('scroll', () => {
+      const scroll = window.scrollY
+
+      if (scroll) navMenu.classList.remove('show-menu')
+
+      scroll >= 400
+        ? header.classList.add('scrolling-header')
+        : header.classList.remove('scrolling-header')
     })
   }
 })
