d678e29d883dc267327c37076eb75514a4a1e6ec
Remy349 <santoraga15@gmail.com>
2023-05-28 23:00:40 -0600

    Added some changes to the index page.

--- flaskr/static/js/main.js
+++ flaskr/static/js/main.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const navToggle = document.getElementById('navToggle')
   const navClose = document.getElementById('navClose')
 
+  const navLogo = document.querySelector('.nav__logo')
+  const navBtn = document.querySelector('.nav__btn')
+  const navLinks = document.querySelectorAll('.nav__link')
+
   if (navToggle) {
     navToggle.addEventListener('click', () => {
       navMenu.classList.add('show-menu')
@@ -25,6 +29,22 @@ document.addEventListener('DOMContentLoaded', () => {
       scroll >= 400
         ? header.classList.add('scrolling-header')
         : header.classList.remove('scrolling-header')
+
+      if (scroll >= 400 && window.innerWidth >= 850) {
+        navLogo.classList.add('scrolling-nav-logo')
+        navBtn.classList.add('scrolling-nav-btn')
+        navLinks.forEach((navLink) => {
+          navLink.classList.add('scrolling-nav-link')
+        })
+      } else if (scroll >= 400 && window.innerWidth < 850) {
+        navLogo.classList.add('scrolling-nav-logo')
+      } else {
+        navLogo.classList.remove('scrolling-nav-logo')
+        navBtn.classList.remove('scrolling-nav-btn')
+        navLinks.forEach((navLink) => {
+          navLink.classList.remove('scrolling-nav-link')
+        })
+      }
     })
   }
 })
